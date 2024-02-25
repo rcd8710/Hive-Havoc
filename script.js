4aaeda8946228ad2d3f3ca4b198762ecb050c0c3
Rited Das <rcd8710@g.rit.edu>
2024-02-24 23:55:43 -0500

    Added pollen functunality

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const myBee = document.getElementById("bee");
+const pollen = document.getElementById("pollen");
 const moveAmount = 10;
 let x = 0;
 let y = 0;
-
 
 function getDistanceToLeft(element) {
     const elementRect = element.getBoundingClientRect();
     const distanceToLeft = elementRect.left;
     return distanceToLeft;
 }
+
+function checkPoll1(posx,posy){
+    if(posx == -660 && posy == 0) {
+        return true;
+    }
+    console.log(x + " " + y);
+}
+
 
 
 document.addEventListener("keydown", event => {
@@ -23,21 +31,21 @@ document.addEventListener("keydown", event => {
                 break;
             case "ArrowLeft":
                 x -= moveAmount;
-
                 if(x <= window.innerWidth * -1) {
                     window.location.href = "Gamescreen.html";
                 }
                 break;
             case "ArrowRight":
                 x += moveAmount;
-                
                 break;
         }
         myBee.style.transform = `translate(${x}px, ${y}px)`;
+        if(checkPoll1(x,y)) {
+            window.location.href = "Homescreen.html";
+        }
+        
     }
 });
-
-
 
 const gamestart = document.getElementById("Gamestart");
 document.addEventListener("keydown", event => {
